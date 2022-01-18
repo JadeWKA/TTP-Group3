df243a5a9b37de4bcaf4b7f0a2644d8689708dea
vincepogz <85046947+vincepogz@users.noreply.github.com>
2022-01-18 14:18:12 -0500

    fixed blur effect

--- Project 2/script.js
+++ Project 2/script.js
@@ -11,13 +11,13 @@ const movieSelect = document.getElementById('movie');
 function closeThis () {
   document.getElementById("page").style.display = "none";
   document.getElementById("carousel-wrapper").style.display = "flex";
-  
+  document.getElementById("carousel-wrapper").style.opacity = "100%";
 }
 
 
 function openThis () {
   document.getElementById("page").style.display = "flex";
-  document.getElementById("carousel-wrapper").style.display = "none";
+  document.getElementById("carousel-wrapper").style.opacity = "50%";
   
 }
 
